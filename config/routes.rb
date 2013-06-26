dbeeeee711d4f5024a0f47bb4e8c0aacf07fadc5
Hoglan <adam.gowland@gmail.com>
2013-06-26 09:18:38 +0100

    Up to 8.2.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 SampleApp::Application.routes.draw do
   #get "users/new"
 	resources :users
+	resources :sessions, only: [:new, :create, :destroy]
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
 	root to: 'static_pages#home'
 	match '/signup',  to: 'users#new'
+	match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
